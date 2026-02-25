88ea36c51dda3a245e206a9f572ede7361ef9dc1
Héctor Ramón Jiménez <hector@hecrj.dev>
2026-02-25 22:46:42 +0100

    Update `hyprland` and `hyprpaper`

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,4 +5,13 @@ if status is-interactive
     # Use gpg-agent for SSH keys
     set -gx SSH_AGENT_PID ""
     set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh"
+
+    if test -f ~/.config/fish/private.fish
+        source ~/.config/fish/private.fish
+    end
+
+    fish_add_path -g ~/.local/bin
+
+    # Enable Mise
+    mise activate fish | source
 end
